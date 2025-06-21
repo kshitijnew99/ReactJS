8cf55091289ce56f5d62baff225ea81a6b64c788
kshitijraoranjannew99@gmail.com <kshitijraoranjannew99@gmail.com>
2025-06-21 20:47:04 +0530

    fine changes

--- project1/src/pages/SingleRecipe.jsx
+++ project1/src/pages/SingleRecipe.jsx
@@ -6,9 +6,23 @@ import { useForm } from "react-hook-form";
 import { toast } from 'react-toastify';
 
 const SingleRecipe = () => {
-    const navigate = useNavigate();
     const  {data,setdata} = useContext(recipecontext)
-    const { register, handleSubmit , reset } = useForm();
+    const param = useParams();
+    var recipe = data.find((recipe) => param.id == recipe.id)
+    const navigate = useNavigate();
+    
+    const { register, handleSubmit , reset } = useForm({
+        defaultValues : {
+        
+            Name : recipe.Name,
+            description : recipe.description,
+            ingredients : recipe.ingredients,
+            instruction : recipe.instruction,
+            category : recipe.category,
+            url : recipe.url
+        
+
+    }});
 
     const SubmitHandler =  (recipe) =>{ 
         var index = data.findIndex((recipe) => param.id == recipe.id)
@@ -20,9 +34,7 @@ const SingleRecipe = () => {
     }
 
     // const { data } = useContext(recipecontext);
-    const param = useParams();
-    var recipe = data.find((recipe) => param.id == recipe.id)
-
+    
     const DeleteHandler = () => {
         var filterdata = data.filter((recipe) => recipe.id !== param.id);
         setdata(filterdata);
@@ -30,72 +42,66 @@ const SingleRecipe = () => {
         navigate("/recipes");
     }
     
-  return (
-    <div className='singlerecipe'>
-        <div className="single-recipe-data">
-            <h1>{recipe.Name}</h1>
-            <img  className='single-img' src={recipe.url} alt="" />
+    return (
+        <div className='singlerecipe'>
+            <div className="single-recipe-data">
+                <h1>{recipe.Name}</h1>
+                <img  className='single-img' src={recipe.url} alt="" />
+            </div>
+
+            <div className="modify-form">
+                <form className="form" onSubmit={handleSubmit(SubmitHandler)} >
+                    <input 
+                    className="url"
+                    {...register("url")}
+                    type="url" 
+                    placeholder='Enter Image URL' /> <br /> <  hr />
+                    <small className="error" >This is how error is shown</small> <br />
+
+                    <input 
+                    className="recipe-title"
+                    {...register("Name")}
+                    type="text" 
+                    placeholder='Recipe Title' /> <br /> <  hr /> <br />
+
+                    <textarea 
+                    className="description"
+                    {...register("description")}
+                    placeholder="// Add the description"  ></textarea>
+                    <hr /><br />
+
+                    <textarea 
+                    className="ingredients"
+                    {...register("ingredients")}
+                    placeholder="// Write ingredients"  ></textarea>
+                    <hr /><br />
+
+                    <textarea 
+                    className="instruction"
+                    {...register("instruction")}
+                    placeholder="// Give instructions"  ></textarea>
+                    <hr /><br />
+
+                    <select 
+                    {...register("categort")}
+                    
+                    className="category">
+                        <option value="breakfast">BreakFast</option>
+                        <option value="lunch">Lunch</option>
+                        <option value="snacks">Snacks</option>
+                        <option value="dinner">Dinner</option>
+                    </select>
+                    <br />
+                    <button className="update-recipe" >Update Recipe</button>
+                    <button type="button" onClick={DeleteHandler} className="delete-recipe" >Delete Recipe</button>
+
+
+                </form>
+            </div>
+            
+            
         </div>
-
-        <div className="modify-form">
-            <form className="form" onSubmit={handleSubmit(SubmitHandler)} >
-                <input 
-                className="url"
-                value={recipe.url}
-                {...register("url")}
-                type="url" 
-                placeholder='Enter Image URL' /> <br /> <  hr />
-                <small className="error" >This is how error is shown</small> <br />
-
-                <input 
-                className="recipe-title"
-                {...register("Name")}
-                value={recipe.Name}
-                type="text" 
-                placeholder='Recipe Title' /> <br /> <  hr /> <br />
-
-                <textarea 
-                className="description"
-                value={recipe.description}
-                {...register("description")}
-                placeholder="// Add the description"  ></textarea>
-                <hr /><br />
-
-                <textarea 
-                className="ingredients"
-                {...register("ingredients")}
-                value={recipe.ingredients}
-                placeholder="// Write ingredients"  ></textarea>
-                <hr /><br />
-
-                <textarea 
-                className="instruction"
-                {...register("instruction")}
-                value={recipe.instruction}
-                placeholder="// Give instructions"  ></textarea>
-                <hr /><br />
-
-                <select 
-                {...register("categort")}
-                
-                className="category">
-                    value={recipe.category}
-                    <option value="breakfast">BreakFast</option>
-                    <option value="lunch">Lunch</option>
-                    <option value="snacks">Snacks</option>
-                    <option value="dinner">Dinner</option>
-                </select>
-                <br />
-                <button className="update-recipe" >Update Recipe</button>
-                <button onClick={DeleteHandler} className="delete-recipe" >Delete Recipe</button>
-
-
-            </form>
-        </div>
-        
-        
-    </div>
-  )
+    )
 }
 
 export default SingleRecipe
