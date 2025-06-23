1326b3555e3ebd159a28c9ee085e8000d744f8dd
kshitijraoranjannew99@gmail.com <kshitijraoranjannew99@gmail.com>
2025-06-23 19:55:51 +0530

    fetch data by axios

--- project1/src/pages/Home.jsx
+++ project1/src/pages/Home.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+
+import axios from './../../node_modules/axios/lib/axios';
 
 const Home = () => {
+
+  const getproduct = async () => {
+          try {
+            const {data} = await axios.get("https://fakestoreapi.com/products ")
+            console.log({data});
+            
+          } catch (error) {
+              console.log(error);
+              
+          }
+  }
   return (
-    <div className='home'>Home</div>
+    <div className='home'>
+      <h1>Home</h1>
+      <button onClick={getproduct} >Get Product</button>
+
+    </div>
   )
 }
 
