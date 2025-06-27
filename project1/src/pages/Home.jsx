8bf9535df951bc8a66e795fe488da44b53fd1eb2
kshitijraoranjannew99@gmail.com <kshitijraoranjannew99@gmail.com>
2025-06-27 22:41:34 +0530

    showing data from API call

--- project1/src/pages/Home.jsx
+++ project1/src/pages/Home.jsx
@@ -2,15 +2,17 @@
 import axios from '../utils/axios';
 import { useEffect } from 'react';
 import { toast } from 'react-toastify';
- 
+ import { useState } from 'react';
 
 const Home = () => {
+  const [products, setProducts] = useState([]);
+
   
 
   const getproduct = async () => {
           try {
             var {data} = await axios.get("/products ");
-          
+            setProducts(data)
 
           } catch (error) {
               // console.log(error);
@@ -31,12 +33,41 @@ const Home = () => {
   
   return (
     <div className='home'>
-      {/* <h1>Home</h1> */}
-      {/* <button onClick={getproduct} >Get Product</button> */}
-      {getproduct}
+      {products.map(product => (
+        <div key={product.id} className="product-card">
+          <img className='pro-img ' src={product.image} alt={product.name} />
+          <h3 className='pro-name' >{product.title}</h3>
+          <p className='pro-price'>₹{product.price}</p>
+          <h4 className='pro-cat'>{product.category}</h4>
+          <h4 className='pro-desc'>{product.description}</h4>
+          
+        </div>
+      ))}
 
     </div>
   )
 }
 
 export default Home
+
+// category
+// : 
+// "men's clothing"
+// description
+// : 
+// "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday"
+// id
+// : 
+// 1
+// image
+// : 
+// "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg"
+// price
+// : 
+// 109.95
+// rating
+// : 
+// {rate: 3.9, count: 120}
+// title
+// : 
+// "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops"
