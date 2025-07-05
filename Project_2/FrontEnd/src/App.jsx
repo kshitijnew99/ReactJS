e481192aa831c0850c97304a79e1839869aeff4e
kshitijraoranjannew99@gmail.com <kshitijraoranjannew99@gmail.com>
2025-07-05 21:51:01 +0530

    connecting the api from our frontend for check if it works

--- Project_2/FrontEnd/src/App.jsx
+++ Project_2/FrontEnd/src/App.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import axios from './api/axios'; // custom axios instance
+import { useEffect } from 'react';
 
 const App = () => {
-  return (
-    <div>hello world</div>
-  )
-}
+  const getproduct = async () => {
+    try {
+      const res = await axios.get('/products');
+      console.log(res); // logs the product list
+    } catch (error) {
+      console.log('Error fetching products:', error);
+    }
+  };
 
-export default App
+  useEffect(() => {
+    getproduct();
+  }, []);
+
+  return <div>hello world</div>;
+};
+
+export default App;
